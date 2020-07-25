8fac338291d4fb8cd82a06f3c3669608513faf88
MaksBelenko <maksim.belenko@gmail.com>
2020-07-25 13:39:47 +0100

    Initial Commit

---
+++ ShimmerTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShimmerTableView
+//
+//  Created by Maksim on 25/07/2020.
+//  Copyright © 2020 Maksim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
